39d7eae046028c2887f6049efccaf34dbbd958f1
prakhar <prtrivedi@q3tech.com>
2020-02-21 17:43:57 +0530

    make deposit and withdraw urls

--- todo/backend/atm/urls.py
+++ todo/backend/atm/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('users/', views.ListTodo.as_view()),
     path('users/<int:pk>/', views.DetailTodo.as_view()),
+    path('users/deposit/<int:card>/<int:amount>', views.deposit),
+    path('users/withdraw/<int:card>/<int:amount>', views.withdraw),
     path('rest-auth/', include('rest_auth.urls')),
 ]
--- todo/backend/atm/views.py
+++ todo/backend/atm/views.py
@@ -1,6 +1,9 @@
 # atm/views.py
 from rest_framework import generics
-
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
 from .models import UserAccount
 from .serializers import UserAccountSerializer
 
@@ -13,3 +16,21 @@ class ListTodo(generics.ListCreateAPIView):
 class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserAccount.objects.all()
     serializer_class = UserAccountSerializer
+
+
+@csrf_exempt
+def deposit(request, card, amount):
+    a = UserAccount.objects.get(card_number=card)
+    a.amount = a.amount+amount
+    a.save()
+    serializer = UserAccountSerializer(a)
+    return JsonResponse(serializer.data, safe=False)
+
+
+@csrf_exempt
+def withdraw(request, card, amount):
+    a = UserAccount.objects.get(card_number=card)
+    a.amount = a.amount-amount
+    a.save()
+    serializer = UserAccountSerializer(a)
+    return JsonResponse(serializer.data, safe=False)
